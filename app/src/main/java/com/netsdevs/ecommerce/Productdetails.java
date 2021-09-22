d49443b0ba38d13bf6b2af65487750a858782bd4
unknown <nets.devs@gmail.com>
2021-09-22 12:33:01 +0300

    E-Commerce App

---
+++ app/src/main/java/com/netsdevs/ecommerce/Productdetails.java
@@ -0,0 +1,13 @@
+package com.netsdevs.ecommerce;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Productdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
